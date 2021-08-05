1d171024334ecb60a7d0424331d82dbb7f93e089
Crowdbotics <team@crowdbotics.com>
2021-08-05 23:54:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "summer_credit_29539Identifier",
+  url: "https://summer_credit_29539Identifier.botics.co",
+  api: "https://summer_credit_29539Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
